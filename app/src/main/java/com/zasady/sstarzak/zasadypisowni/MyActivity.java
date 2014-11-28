f907e45dc414d6f3ac75a02015c4ded36e095569
Szymon Starzak <szymonstarzak@gmail.com>
2014-11-29 00:30:44 +0100

    Przy zapisie i dodaniu relacji nalezy podawac w konstruktorze zapisany juz obiekt

--- app/src/main/java/com/zasady/sstarzak/zasadypisowni/MyActivity.java
+++ app/src/main/java/com/zasady/sstarzak/zasadypisowni/MyActivity.java
@@ -7,7 +7,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-import java.util.Iterator;
 import java.util.List;
 
 
@@ -20,17 +19,18 @@ public class MyActivity extends Activity {
         setContentView(R.layout.activity_my);
         context = getApplicationContext();
 
+        this.deleteDatabase("zasady.db");
         seedDataIntoDataBase();
 
         TextView tw = (TextView) findViewById(R.id.textView);
 
-        Alphabet a = new Alphabet("u");
+        //Alphabet a = new Alphabet("u");
         List<Rule> list = Rule.listAll(Rule.class);//, "alphabet = ?", new String[]{String.valueOf(a)});
 
 
         for( Rule b:list) {
-            //if(b.alphabet.pl_str == "u")
-                tw.append(b.alphabet.pl_str);
+            if(b.alphabet.pl_str.equals("u"))
+                tw.append(" " + b.principle );
         }
 
        // Alphabet a2 = Alphabet.findById(Alphabet.class,3l);
@@ -54,19 +54,18 @@ public class MyActivity extends Activity {
     }
     public void seedDataIntoDataBase() {
         Alphabet.deleteAll(Alphabet.class);
-        Alphabet a;
-        a = new Alphabet("rz"); a.save();
-        a = new Alphabet("ż");  a.save(); //a.setId(2l);
-        a = new Alphabet("u");  a.save(); //a.setId(3l);
-        a = new Alphabet("ó");  a.save(); //a.setId(4l);
-        a = new Alphabet("ch"); a.save(); //a.setId(5l);
-        a = new Alphabet("h");  a.save(); //a.setId(6l);
+        Alphabet a1 = new Alphabet("rz"); a1.save();
+        Alphabet a2 = new Alphabet("ż");  a2.save(); //a.setId(2l);
+        Alphabet a3 = new Alphabet("u");  a3.save(); //a.setId(3l);
+        Alphabet a4 = new Alphabet("ó");  a4.save(); //a.setId(4l);
+        Alphabet a5 = new Alphabet("ch"); a5.save(); //a.setId(5l);
+        Alphabet a6 = new Alphabet("h");  a6.save(); //a.setId(6l);
 
-        Rule r;
-        r = new Rule("w wyrazach wymienia sie na R", new Alphabet("rz")); r.save();
-        r = new Rule("Rz piszemy w zakończeniach wyrazów: -arz, -erz", new Alphabet("rz")); r.save();
-        r = new Rule("Rz piszemy po spółgłoskach: b, p, d, t, g, k, ch, j, w", new Alphabet("rz")); r.save();
-        r = new Rule("w zakończeniach rzeczowników: un, unek, uchna, uszka, uszek, uch, us, usia", new Alphabet("u")); r.save();
+
+       Rule r1 = new Rule("w wyrazach wymienia sie na R", a1); r1.save();
+       Rule r2 = new Rule("Rz piszemy w zakończeniach wyrazów: -arz, -erz", a1); r2.save();
+       Rule r3 = new Rule("Rz piszemy po spółgłoskach: b, p, d, t, g, k, ch, j, w", a1); r3.save();
+       Rule r4 = new Rule("w zakończeniach rzeczowników: un, unek, uchna, uszka, uszek, uch, us, usia", a3); r4.save();
     }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {

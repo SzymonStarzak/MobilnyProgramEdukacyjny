5f46ba3f7ac27a46d35f79095096c0a932f86348
Szymon Starzak <szymonstarzak@gmail.com>
2014-12-20 16:35:54 +0100

    Dodano funkcjonalnosc dla Szybkich operacji

--- app/src/main/java/com/zasady/sstarzak/zasadypisowni/FastOperationActivity.java
+++ app/src/main/java/com/zasady/sstarzak/zasadypisowni/FastOperationActivity.java
@@ -6,6 +6,7 @@ import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.apache.commons.jexl2.Expression;
 import org.apache.commons.jexl2.JexlEngine;
@@ -16,13 +17,17 @@ import java.util.Random;
 
 public class FastOperationActivity extends Activity implements View.OnClickListener{
 
+    CountDownTimer cdt;
+
+    Integer cdt_time;
+
     TextView number1_tv;
 
     TextView number2_tv;
 
     TextView number3_tv;
 
-    TextView answer_tv;
+    TextView correct_number_tv;
 
     TextView operator1_tv;
 
@@ -40,23 +45,29 @@ public class FastOperationActivity extends Activity implements View.OnClickListe
 
     Button button_mod;
 
-    Button button_pow;
-
-    Button button_sqrt;
-
-    Button button_minus_sqrt;
-
     Integer number1;
 
     Integer number2;
 
     Integer number3;
 
+    Integer correct_number;
+
     Integer answer_number;
+
+    Integer operators_count;
+
+    Integer max_number;
 
     Integer operator1;
 
     Integer operator2;
+
+    Integer attempts;
+
+    String sOperator1;
+
+    String sOperator2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,7 +77,7 @@ public class FastOperationActivity extends Activity implements View.OnClickListe
         number1_tv = (TextView) findViewById(R.id.number1_tv);
         number2_tv = (TextView) findViewById(R.id.number2_tv);
         number3_tv = (TextView) findViewById(R.id.number3_tv);
-        answer_tv = (TextView) findViewById(R.id.answer_number_tv);
+        correct_number_tv = (TextView) findViewById(R.id.correct_number_tv);
         operator1_tv = (TextView) findViewById(R.id.operator1_tv);
         operator2_tv = (TextView) findViewById(R.id.operator2_tv);
 
@@ -82,11 +93,16 @@ public class FastOperationActivity extends Activity implements View.OnClickListe
         button_div.setOnClickListener(this);
         button_mod.setOnClickListener(this);
 
+        operators_count = 5;
+        max_number = 15;
+
+        cdt_time = 15000;
+
         fastOperationGame();
 
     }
     public void fastOperationGame() {
-        CountDownTimer cdt = new CountDownTimer(9000, 100) {
+            cdt = new CountDownTimer(cdt_time, 100) {
             @Override
             public void onTick(long l) {
                 countdown_tv.setText(String.valueOf(l / 1000));
@@ -94,59 +110,25 @@ public class FastOperationActivity extends Activity implements View.OnClickListe
 
             @Override
             public void onFinish() {
-                countdown_tv.setText("0");
+                countdown_tv.setText("0"); onIncorrectAnswer();
             }
         }.start();
 
         Random r = new Random();
 
-        number1 = r.nextInt(15)+1;
-        number2 = r.nextInt(15)+1;
-        number3 = r.nextInt(15)+1;
+        attempts = 2;
 
-        operator1 = r.nextInt(5);
-        operator2 = r.nextInt(5);
+        number1 = r.nextInt(2 * max_number)-15;
+        number2 = r.nextInt(max_number)+1;
+        number3 = r.nextInt(max_number)+1;
 
-        String sOperator1="";
-        switch (operator1) {
-            case 0: sOperator1 = "+"; break;
-            case 1: sOperator1 = "-"; break;
-            case 2: sOperator1 = "*"; break;
-            case 3: sOperator1 = "/"; break;
-            case 4: sOperator1 = "%"; break;
-        }
-
-        String sOperator2="";
-        switch (operator2) {
-            case 0: sOperator2 = "+"; break;
-            case 1: sOperator2 = "-"; break;
-            case 2: sOperator2 = "*"; break;
-            case 3: sOperator2 = "/"; break;
-            case 4: sOperator2 = "%"; break;
-        }
-
-        //Korekta wylosowanych liczb dla operacji dzielenia
-      /*  if (operator2 == 3 && operator1 != 3) {
-            while (number2 % number3 != 0){
-                number2= r.nextInt(30)+1;
-            }
-        }
-       else if (operator1 == 3 && operator2 != 3) {
-           while (number1 % number2 != 0){
-               number1=r.nextInt(30)+1;
-           }
-       }
-       else if (operator1 == 3 && operator2 == 3){
-            while (number1 % (number2 * number3) != 0){
-                number1++;
-            }
-        }*/
+        operator1 = r.nextInt(operators_count);
+        operator2 = r.nextInt(operators_count);
 
         JexlEngine jexl = new JexlEngine();
-        Expression func = jexl.createExpression("x1"+sOperator1+"x2"+sOperator2+"x3");
+        Expression func = jexl.createExpression("x1"+getSymbolicOperator(operator1)+"x2"+getSymbolicOperator(operator2)+"x3");
         MapContext mc = new MapContext();
 
-       // answer_number =  Integer.valueOf(func.evaluate(mc).toString());
         mc.set("x1", number1);
         mc.set("x2", number2);
         mc.set("x3", number3);
@@ -155,25 +137,82 @@ public class FastOperationActivity extends Activity implements View.OnClickListe
         number2_tv.setText(String.valueOf(number2));
         number3_tv.setText(String.valueOf(number3));
 
-        operator1_tv.setText(sOperator1);
-        operator2_tv.setText(sOperator2);
+        operator1_tv.setText("?");
+        operator2_tv.setText("?");
 
-        answer_number = Integer.parseInt(func.evaluate(mc).toString());
-        answer_tv.setText(String.valueOf(answer_number));
-      //  answer_tv.setText(func.evaluate(mc).toString());
-
+        correct_number = Integer.parseInt(func.evaluate(mc).toString());
+        correct_number_tv.setText(String.valueOf(correct_number));
     }
 
-    public void fastOperationTest() {
+    public void fastOperationTest(String sOperator1, String sOperator2) {
 
+        JexlEngine jexl = new JexlEngine();
+        Expression func = jexl.createExpression("x1"+sOperator1+"x2"+sOperator2+"x3");
+        MapContext mc = new MapContext();
+
+        mc.set("x1", number1);
+        mc.set("x2", number2);
+        mc.set("x3", number3);
+
+        answer_number = Integer.parseInt(func.evaluate(mc).toString());
+
+        if (answer_number.equals(correct_number)) {
+            onCorrectAnswer();
+        }
+        else {
+            onIncorrectAnswer();
+        }
+    }
+
+    public void onCorrectAnswer() {
+        Toast.makeText(this,"Dobrze",Toast.LENGTH_SHORT).show();
+        cdt.cancel();
+        fastOperationGame();
+    }
+    public void onIncorrectAnswer() {
+        Toast.makeText(this,"Źle " + getSymbolicOperator(operator1) + " " + getSymbolicOperator(operator2),Toast.LENGTH_LONG).show();
+        cdt.cancel();
+        fastOperationGame();
+    }
+    public String getSymbolicOperator(int operator) {
+        switch (operator) {
+            case 0: return "+";
+            case 1: return "-";
+            case 2: return "*";
+            case 3: return "/";
+            case 4: return "%";
+        }
+        return "";
     }
     @Override
     public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.button_plus: break;
-            case R.id.button_minus: break;
-            case R.id.button_times: break;
-            case R.id.button_div: break;
+
+        if(attempts == 2) {
+            switch (view.getId()) {
+                case R.id.button_plus: sOperator1 = getSymbolicOperator(0); break;
+                case R.id.button_minus: sOperator1 = getSymbolicOperator(1); break;
+                case R.id.button_times: sOperator1 = getSymbolicOperator(2); break;
+                case R.id.button_div: sOperator1 = getSymbolicOperator(3); break;
+                case R.id.button_mod: sOperator1 = getSymbolicOperator(4); break;
+            }
+            operator1_tv.setText(sOperator1);
+            attempts--;
+        } else if( attempts == 1) {
+            switch (view.getId()) {
+                case R.id.button_plus: sOperator2 = getSymbolicOperator(0); break;
+                case R.id.button_minus: sOperator2 = getSymbolicOperator(1); break;
+                case R.id.button_times: sOperator2 = getSymbolicOperator(2); break;
+                case R.id.button_div: sOperator2 = getSymbolicOperator(3); break;
+                case R.id.button_mod: sOperator2 = getSymbolicOperator(4); break;
+            }
+            operator2_tv.setText(sOperator2);
+            attempts--;
         }
+
+        if (attempts == 0) {
+            fastOperationTest(sOperator1,sOperator2);
+        }
+
+
     }
 }

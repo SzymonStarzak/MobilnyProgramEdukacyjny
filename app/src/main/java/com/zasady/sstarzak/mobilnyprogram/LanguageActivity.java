2188670a82526728beb4ee74a581f1dc3dfbe376
Szymon Starzak <szymonstarzak@gmail.com>
2014-12-31 02:04:21 +0100

    Dodano listView dla menu jezyka i przedmiotow scislych

--- app/src/main/java/com/zasady/sstarzak/mobilnyprogram/LanguageActivity.java
+++ app/src/main/java/com/zasady/sstarzak/mobilnyprogram/LanguageActivity.java
@@ -1,56 +1,116 @@
 package com.zasady.sstarzak.mobilnyprogram;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Button;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 
-public class LanguageActivity extends Activity {
+public class LanguageActivity extends Activity implements AdapterView.OnItemClickListener {
+
+    ListView list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_language);
 
-        Button button1 = (Button) findViewById(R.id.ortography_button);
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), OrthographyActivity.class);
-                startActivity(intent);
-            }
-        });
+        list = (ListView) findViewById(R.id.listView_language);
+        list.setAdapter(new MyAdapter(this));
+        list.setOnItemClickListener(this);
 
-        Button button2 = (Button) findViewById(R.id.hangman_button);
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), HangmanActivity.class);
-                intent.putExtra("language","polish");
-                startActivity(intent);
-            }
-        });
 
-        Button button3 = (Button) findViewById(R.id.hangman_eng_button);
-        button3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), HangmanActivity.class);
-                intent.putExtra("language","english");
-                startActivity(intent);
-            }
-        });
-
-        Button button4 = (Button) findViewById(R.id.english_words);
-        button4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), EnglishActivity.class);
-                startActivity(intent);
-            }
-        });
     }
 
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        switch (i) {
+            case 0:
+                Intent intent = new Intent(getApplicationContext(), OrthographyActivity.class);
+                startActivity(intent);
+                break;
+            case 1:
+                intent = new Intent(getApplicationContext(), HangmanActivity.class);
+                intent.putExtra("language","polish");
+                startActivity(intent);
+                break;
+            case 2:
+                intent = new Intent(getApplicationContext(), HangmanActivity.class);
+                intent.putExtra("language","english");
+                startActivity(intent);
+                break;
+            case 3:
+                intent = new Intent(getApplicationContext(), EnglishActivity.class);
+                startActivity(intent);
+                break;
+        }
+    }
+}
+class SingleRow {
+    String title;
+    String desc;
+    int image;
+
+    SingleRow(String title, String desc, int image) {
+        this.title = title;
+        this.desc = desc;
+        this.image = image;
+    }
+}
+class MyAdapter extends BaseAdapter {
+    ArrayList<SingleRow> list;
+    Context context;
+    MyAdapter(Context c) {
+        list = new ArrayList<SingleRow>();
+        Resources res = c.getResources();
+        context = c;
+        String [] titles = res.getStringArray(R.array.titles_language);
+        String [] descs = res.getStringArray(R.array.descs_language);
+        int [] images = {R.drawable.pioro,R.drawable.wisielec,R.drawable.wisielec_eng,R.drawable.angielski};
+
+        for(int i = 0; i < 4; i++) {
+            list.add(new SingleRow(titles[i],descs[i],images[i]));
+        }
+    }
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return list.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View row = inflater.inflate(R.layout.singel_row,viewGroup,false);
+        TextView title = (TextView) row.findViewById(R.id.textView);
+        TextView desc = (TextView) row.findViewById(R.id.textView2);
+        ImageView image = (ImageView) row.findViewById(R.id.imageView);
+
+        SingleRow temp = list.get(i);
+        title.setText(temp.title);
+        desc.setText(temp.desc);
+        image.setImageResource(temp.image);
+
+        return row;
+    }
 }
--- app/src/main/java/com/zasady/sstarzak/mobilnyprogram/NumberActivity.java
+++ app/src/main/java/com/zasady/sstarzak/mobilnyprogram/NumberActivity.java
@@ -1,57 +1,115 @@
 package com.zasady.sstarzak.mobilnyprogram;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.Button;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 
-public class NumberActivity extends Activity {
+public class NumberActivity extends Activity implements AdapterView.OnItemClickListener {
+
+    ListView list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_number);
 
-        Button b1 = (Button) findViewById(R.id.fast_operation_button);
-        b1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(),FastOperationActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        Button b2 = (Button) findViewById(R.id.periodic_table_button);
-        b2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(),PeriodicTableActivity.class);
-                intent.putExtra("language","polish");
-                startActivity(intent);
-            }
-        });
-
-        Button b3 = (Button) findViewById(R.id.eng_periodic_table_button);
-        b3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(),PeriodicTableActivity.class);
-                intent.putExtra("language","english");
-                startActivity(intent);
-            }
-        });
-
-        Button b4 = (Button) findViewById(R.id.numeral_game_button);
-        b4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(),NumeralSystemActivity.class);
-                startActivity(intent);
-            }
-        });
+        list = (ListView) findViewById(R.id.listView_science);
+        list.setAdapter(new MyAdapter2(this));
+        list.setOnItemClickListener(this);
 
     }
 
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        switch (i) {
+            case 0:
+                Intent intent = new Intent(getApplicationContext(),FastOperationActivity.class);
+                startActivity(intent);
+                break;
+            case 1:
+                intent = new Intent(getApplicationContext(),PeriodicTableActivity.class);
+                intent.putExtra("language","polish");
+                startActivity(intent);
+                break;
+            case 2:
+                intent = new Intent(getApplicationContext(),PeriodicTableActivity.class);
+                intent.putExtra("language","english");
+                startActivity(intent);
+                break;
+            case 3:
+                intent = new Intent(getApplicationContext(),NumeralSystemActivity.class);
+                startActivity(intent);
+                break;
+        }
+    }
+}
+class SingleRow2 {
+    String title;
+    String desc;
+    int image;
+
+    SingleRow2(String title, String desc, int image) {
+        this.title = title;
+        this.desc = desc;
+        this.image = image;
+    }
+}
+class MyAdapter2 extends BaseAdapter {
+    ArrayList<SingleRow2> list;
+    Context context;
+    MyAdapter2(Context c) {
+        list = new ArrayList<SingleRow2>();
+        Resources res = c.getResources();
+        context = c;
+        String [] titles = res.getStringArray(R.array.titles_science);
+        String [] descs = res.getStringArray(R.array.descs_science);
+        int [] images = {R.drawable.math_operations,R.drawable.chemical,R.drawable.chemical_eng,R.drawable.numeral};
+
+        for(int i = 0; i < 4; i++) {
+            list.add(new SingleRow2(titles[i],descs[i],images[i]));
+        }
+    }
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return list.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View row = inflater.inflate(R.layout.singel_row,viewGroup,false);
+        TextView title = (TextView) row.findViewById(R.id.textView);
+        TextView desc = (TextView) row.findViewById(R.id.textView2);
+        ImageView image = (ImageView) row.findViewById(R.id.imageView);
+
+        SingleRow2   temp = list.get(i);
+        title.setText(temp.title);
+        desc.setText(temp.desc);
+        image.setImageResource(temp.image);
+
+        return row;
+    }
 }
